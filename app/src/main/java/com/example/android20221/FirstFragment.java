fa3452f8d1a1e1df94b5253752d32f8e7d2bfe94
Roberto Franco <roberto.yuri.franco@gmail.com>
2022-11-08 20:31:09 -0300

    GPS

--- app/src/main/java/com/example/android20221/FirstFragment.java
+++ app/src/main/java/com/example/android20221/FirstFragment.java
@@ -1,25 +1,69 @@
 package com.example.android20221;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.android20221.databinding.FragmentFirstBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        fusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(
+                        this.getContext());
+        // Inflate the layout for this fragment
+        if (ActivityCompat.checkSelfPermission(this.getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this.getActivity(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            //return "";
+        }
+        Log.d("location32",
+                fusedLocationClient.getLastLocation().toString());
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this.getActivity(),
+                        new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    // Logic to handle location object
+                                }
+                                Log.d("location", location.toString());
+                            }
+                        });
+        Log.d("location32", ""+fusedLocationClient.getLastLocation().toString());
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
